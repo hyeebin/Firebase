d1b2e01d83f0d7308c70aeaedac6fa6e301d6021
hyebin <gpqlsdbwls1@naver.com>
2023-06-20 13:44:45 +0900

    Initial Commit

---
+++ Firebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase
+//
+//  Created by cubicinc on 2023/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
